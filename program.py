3a1a5d93c1e6b38cdc52c6473cabb65bb63f5fcf
sujithkumar216802 <sujithkumar216802@gmail.com>
2021-08-30 19:39:46 +0530

    [python] generate unique repo name

--- program.py
+++ program.py
@@ -1,16 +1,35 @@
 import json
 import http.client
+import uuid
 
 config = json.load(open('config.json'))
 
 conn = http.client.HTTPSConnection("api.github.com")
-payload = "{\"query\":\"{  user(login: \\\"sujithkumar216802\\\") {    contributionsCollection{\\n      contributionCalendar{\\n        weeks{\\n          contributionDays {\\n            date\\ncontributionCount\\n          }\\n        }\\n      }\\n    }  }}\"}"
 headers = {
   'Content-Type': 'application/json',
   'Authorization': 'Bearer {}'.format(config['access_token']),
   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
 }
+
+
+# commit count
+payload = "{\"query\":\"{  user(login: \\\"%s\\\") {    contributionsCollection{\\n      contributionCalendar{\\n        weeks{\\n          contributionDays {\\n            date\\ncontributionCount\\n          }\\n        }\\n      }\\n    }  }}\"}" % (config['user_name'])
 conn.request("POST", "/graphql", payload, headers)
 res = conn.getresponse()
-data = res.read()
-print(data)
+count_data = json.loads(res.read().decode('utf-8'))
+
+
+#reponame
+
+name_is_not_unique = True
+
+while name_is_not_unique:
+    repo_name = str(uuid.uuid4().hex)
+
+    #repo exixts
+    payload = "{\"query\":\"{\\n  user(login: \\\"%s\\\") {\\n    repository(name:\\\"%s\\\") {\\n      description\\n    }\\n  }\\n}\"" % (config['user_name'], repo_name)
+    conn.request("POST", "/graphql", payload, headers)
+    res = conn.getresponse()
+    repo_data = json.loads(res.read().decode('utf-8'))
+    if repo_data['data']['user']['repository']==None:
+        name_is_not_unique = False

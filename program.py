c7e72c8b90c3fbe7a1ac6412244f626d7bc5d63c
sujithkumar216802 <sujithkumar216802@gmail.com>
2021-08-30 21:09:36 +0530

    [python] calculate required commits per day

--- program.py
+++ program.py
@@ -6,30 +6,42 @@ config = json.load(open('config.json'))
 
 conn = http.client.HTTPSConnection("api.github.com")
 headers = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer {}'.format(config['access_token']),
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {}'.format(config['access_token']),
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
 }
 
 
 # commit count
-payload = "{\"query\":\"{  user(login: \\\"%s\\\") {    contributionsCollection{\\n      contributionCalendar{\\n        weeks{\\n          contributionDays {\\n            date\\ncontributionCount\\n          }\\n        }\\n      }\\n    }  }}\"}" % (config['user_name'])
+payload = "{\"query\":\"{  user(login: \\\"%s\\\") {    contributionsCollection{\\n      contributionCalendar{\\n        weeks{\\n          contributionDays {\\n            date\\ncontributionCount\\n          }\\n        }\\n      }\\n    }  }}\"}" % (
+    config['user_name'])
 conn.request("POST", "/graphql", payload, headers)
 res = conn.getresponse()
 count_data = json.loads(res.read().decode('utf-8'))
+weeks = count_data['data']['user']['contributionsCollection']['contributionCalendar']['weeks']
 
+new_count = []
 
-#reponame
+for week in weeks:
+    for day in week['contributionDays']:
+        if(day['contributionCount'] < config['commits_per_day']):
+            new_count.append(
+                {'date': day['date'], 'commits': config['commits_per_day']-day['contributionCount']})
 
+if len(new_count) == 0:
+    exit()
+
+# reponame
 name_is_not_unique = True
 
 while name_is_not_unique:
     repo_name = str(uuid.uuid4().hex)
 
-    #repo exixts
-    payload = "{\"query\":\"{\\n  user(login: \\\"%s\\\") {\\n    repository(name:\\\"%s\\\") {\\n      description\\n    }\\n  }\\n}\"" % (config['user_name'], repo_name)
+    # repo exixts
+    payload = "{\"query\":\"{\\n  user(login: \\\"%s\\\") {\\n    repository(name:\\\"%s\\\") {\\n      description\\n    }\\n  }\\n}\"" % (
+        config['user_name'], repo_name)
     conn.request("POST", "/graphql", payload, headers)
     res = conn.getresponse()
     repo_data = json.loads(res.read().decode('utf-8'))
-    if repo_data['data']['user']['repository']==None:
+    if repo_data['data']['user']['repository'] == None:
         name_is_not_unique = False
